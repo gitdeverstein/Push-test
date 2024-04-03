94d8a6575a12cc343078e04a9caaea47f99c0a7a
HEI-Andy <hei.andy.8@gmail.com>
2024-04-04 00:36:44 +0300

    feat: add Robot.js

---
+++ Robot.js
@@ -0,0 +1,30 @@
+// Fonction pour déplacer le robot
+function deplacerRobot(position, direction) {
+    switch (direction) {
+        case 'haut':
+            return { x: position.x, y: position.y - 1 };
+        case 'bas':
+            return { x: position.x, y: position.y + 1 };
+        case 'gauche':
+            return { x: position.x - 1, y: position.y };
+        case 'droite':
+            return { x: position.x + 1, y: position.y };
+        default:
+            console.error('Direction invalide');
+            return position;
+    }
+}
+
+let positionRobot = { x: 0, y: 0 };
+
+positionRobot = deplacerRobot(positionRobot, 'haut');
+console.log(positionRobot);
+
+positionRobot = deplacerRobot(positionRobot, 'droite');
+console.log(positionRobot);
+
+positionRobot = deplacerRobot(positionRobot, 'bas');
+console.log(positionRobot);
+
+positionRobot = deplacerRobot(positionRobot, 'gauche');
+console.log(positionRobot);
